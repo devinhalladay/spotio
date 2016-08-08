2e3e9406873959496fc4f2bea384fc664b1ef9c5
Devin Halladay <devin@devinhalladay.com>
2016-08-07 21:33:25 -0400

    Sass/file renaming and moving now working

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,37 @@
 module.exports = function(grunt) {
-  // Do grunt-related things in here
+  grunt.initConfig({
+    sass: {
+      glue: {
+        files: [{
+          expand: true,
+          cwd: "skin",
+          src: ["glue1.scss"],
+          dest: "dist",
+          ext: ".css"
+        }]
+      },
+      apps: {
+        files: [{
+          expand: true,
+          cwd: "skin/apps",
+          src: ["*.scss"],
+          dest: "test/apps/",
+          ext: ".css",
+          rename: function(dest, src) {
+            return dest + src.slice(0, -4) + "/" + 'style.css';
+          }
+        }]
+      },
+    },
+    watch: {
+      source: {
+        files: ['skin/**/*.scss'],
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.registerTask('default', ['sass']);
 };
