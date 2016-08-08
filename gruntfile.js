7ec84de5e9fd1352690f78615c1f0b17275bb427
Devin Halladay <devin@devinhalladay.com>
2016-08-07 22:31:29 -0400

    BOOM! COMPILATION IS WORKING

--- gruntfile.js
+++ gruntfile.js
@@ -1,26 +1,49 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // Compile Sass
     sass: {
-      task: {
-        src: ['source'],
-        dest: 'destination'
-      },
       options: {
-        'sourcemap': 'none',
-        'update': true
+        sourceMap: false,
+      },
+      glue: {
+        files: [{
+          expand: true,
+          cwd: "skin",
+          src: ["glue1.scss"],
+          dest: "dist",
+          ext: ".css"
+        }]
+      },
+      apps: {
+        files: [{
+          expand: true,
+          cwd: "skin/apps",
+          src: ["*.scss"],
+          dest: "Apps/",
+          ext: ".css",
+          rename: function(dest, src) {
+            return dest + src.slice(0, -4) + "/css/" + 'style.css';
+          }
+        }]
       }
     },
+
+    // Watch and build
     watch: {
-      task: {
-        src: ['source'],
-        dest: 'destination'
+      sass: {
+        files: 'skin/**/*.scss',
+        tasks: ['sass']
       }
     }
+
   });
 
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  // Load dependencies
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
 
+  // Run tasks
   grunt.registerTask('default', ['sass', 'watch']);
 };
